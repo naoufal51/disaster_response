0e7ac65609e6da993b229b0dd1b61a6f8e63b76f
naoufal51 <naoufal51@gmail.com>
2023-07-27 03:17:36 +0100

    Update run.py

--- app/run.py
+++ app/run.py
@@ -160,9 +160,10 @@ def go():
     )
 
 
-def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+# def main():
+#     app.run(host='0.0.0.0', port=3001, debug=True)
 
 
 if __name__ == '__main__':
-    main()
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=3001)
